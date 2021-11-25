643c547e8e6df1c756dbb7e84233bc9bc5c3c02b
DWarez <ldwarezl@gmail.com>
2021-11-25 15:08:55 +0100

    Fixed messages structure, implemented test files for rosbag

--- src/runtime/src/rosbag_write.cpp
+++ src/runtime/src/rosbag_write.cpp
@@ -1,19 +1,18 @@
 #include <ros/ros.h>
 #include <rosbag/bag.h>
-#include <std_msgs/UInt8.h>
-#include <std_msgs/String.h>
 #include <runtime/Custom_Message.h>
 
 
 class Listener {
     public:
-        std_msgs::UInt8 current_status;
+        runtime::Custom_Message value;
         void callback(const runtime::Custom_Message& msg);
 };
 
 
 void Listener::callback(const runtime::Custom_Message& msg) {
-    this->current_status = msg.number;
+    ROS_INFO("Storing a message: %d, %d", msg.version, msg.number);
+    this->value = msg;
 }
 
 
@@ -21,17 +20,19 @@ int main(int argc, char **argv) {
     Listener listener;
 
     rosbag::Bag bag;
-    bag.open("test.bag", rosbag::bagmode::Write);
+    bag.open("logs/test.bag", rosbag::bagmode::Write);
 
     ros::init(argc, argv, "test_bag");
     ros::NodeHandle nh;
 
     // Subscribe transition topics
-    ros::Subscriber sub = nh.subscribe("custom_message_status_simulated", 1, &Listener::callback, &listener);
+    ros::Subscriber sub = nh.subscribe("custom_message", 1, &Listener::callback, &listener);
 
-    bag.write("ebs_log", ros::Time::now(), listener.current_status);
+    while(ros::ok()) {
+        bag.write("rosbag_test", ros::Time::now(), listener.value);
 
-    ros::spin();
+        ros::spin();
+    }
 
     bag.close();
 
